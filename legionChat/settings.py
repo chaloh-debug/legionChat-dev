fbdab2b27a8a2fdac74da4f081361397b28eb38f
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-11-03 02:20:33 +0300

    db config

--- legionChat/settings.py
+++ legionChat/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,6 +104,8 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
